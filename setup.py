841180ea6714d01b156eb57f29181e9edbb2fcd0
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-01-05 22:49:18 -0500

    Link to GitHub page.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     py_modules=['lektor_atom'],
     install_requires=['MarkupSafe', 'Lektor'],
     tests_require=['lxml', 'pytest'],
+    url='https://github.com/ajdavis/lektor-atom',
     entry_points={
         'lektor.plugins': [
             'atom = lektor_atom:AtomPlugin',
