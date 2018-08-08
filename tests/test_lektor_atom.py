ad6502f5fe19662ff0ac498337cf3a5ce6b29776
Sebastian Reuße <seb@wirrsal.net>
2018-08-08 19:15:20 +0200

    Add alternatives support
    While lektor-atom currently supports creating feed variants by relying on
    specially-crafted item query expressions, this is only useful for item model
    fields that contain natural language strings. When creating feeds based on
    structured data, this is not helpful, since field values are identical across
    two alts.
    
    E.g., when we would like to publish a feed based on PDF attachments that have a
    `volume_number` set, we are currently unable to have the English feed display
    item titles as »Volume 1« in the English feed resp. »Ausgabe 1« in the German
    alternative.
    
    This commit adds support for such use-cases by adding two new mechanisms:
    
    1. Instead of supplying field names to map records to Atom entries, the user
       supplies a Jinja template. These expressions are evaluated with `this` bound
       to the blog resp. the item record.
    
    2. For a feed named `feed`, configuration values are first looked-up in the
       config file section `[feed.ALT]`, where `ALT` is the alternative currently
       being generated. This allows settings defaults in `[feed]`, and overriding
       only those settings that are locale-specific by adding them to `[feed.ALT]`.
    
    As a side-effect, this also benefits users that don’t use alternatives, since it
    enables them to compose item titles, bodies, etc. using multiple fields at the
    same time.
    
    Fixes #3, #13.

--- tests/test_lektor_atom.py
+++ tests/test_lektor_atom.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 from lektor.context import Context
@@ -12,7 +14,7 @@ def test_typical_feed(pad, builder):
     
     assert 'Feed One' == feed.title
     assert 'My Summary' == feed.subtitle
-    assert 'text' == feed.subtitle.attrib['type']
+    assert 'html' == feed.subtitle.attrib['type']
     assert 'A. Jesse Jiryu Davis' == feed.author.name
     assert 'http://x.com/typical-blog/' == feed.link[0].attrib['href']
     assert 'http://x.com/typical-blog/feed.xml' == feed.link[1].attrib['href']
@@ -78,6 +80,39 @@ def test_custom_feed(pad, builder):
     assert 'A. Jesse Jiryu Davis' == post1.author.name
 
 
+def test_multilang_feed(pad, builder):
+    failures = builder.build_all()
+    assert not failures
+
+    feed_path = os.path.join(builder.destination_path,
+                             'de/multilang-blog/feed.xml')
+    feed = objectify.parse(open(feed_path)).getroot()
+
+    assert u'Feed Fünf' == feed.title
+    assert 'http://x.com/de/multilang-blog/' \
+        == feed.link[0].attrib['href']
+    assert 'http://x.com/de/multilang-blog/feed.xml' \
+        == feed.link[1].attrib['href']
+    assert feed.entry.title == 'Post 2 (13.12.2015)'
+
+    base = feed.entry.attrib['{http://www.w3.org/XML/1998/namespace}base']
+    assert 'http://x.com/de/multilang-blog/post2/' == base
+
+    feed_path = os.path.join(builder.destination_path,
+                             'multilang-blog/feed.xml')
+    feed = objectify.parse(open(feed_path)).getroot()
+
+    assert 'Feed Five' == feed.title
+    assert 'http://x.com/multilang-blog/' \
+        == feed.link[0].attrib['href']
+    assert 'http://x.com/multilang-blog/feed.xml' \
+        == feed.link[1].attrib['href']
+    assert feed.entry.title == 'Post 2 (Dec 13, 2015)'
+
+    base = feed.entry.attrib['{http://www.w3.org/XML/1998/namespace}base']
+    assert 'http://x.com/multilang-blog/post2/' == base
+
+
 def test_virtual_resolver(pad, builder):
     # Pass a virtual source path to url_to().
     feed_path = '/typical-blog@atom/feed-one'
@@ -97,6 +132,10 @@ def test_virtual_resolver(pad, builder):
     assert feed_instance and feed_instance.feed_name == 'Feed Three'
     url_path = pad.get('custom-blog/post1').url_to(feed_instance)
     assert url_path == '../../custom-blog/atom.xml'
+
+    feed_instance = pad.get('multilang-blog@atom/feed-five', alt='de')
+    assert feed_instance and feed_instance.feed_name == u'Feed Fünf'
+    assert feed_instance.url_path == '/de/multilang-blog/feed.xml'
 
 
 def test_dependencies(pad, builder, reporter):
@@ -126,19 +165,20 @@ def feeds_from_template(pad, template):
 
 def test_discover_all(pad):
     template = r'''
-    {% for feed in atom_feeds() %}
+    {% for feed in atom_feeds(alt='_primary') %}
         {{ feed.feed_id }}
     {% endfor %}
     '''
     all_feeds = set(['feed-one', 'feed-two',
-                     'feed-three', 'feed-four'])
+                     'feed-three', 'feed-four',
+                     'feed-five'])
     feeds_discovered = feeds_from_template(pad, template)
     assert feeds_discovered == all_feeds
 
 
 def test_discover_local(pad):
     template_blog = r'''
-    {% for feed in atom_feeds(for_page=site.get('/custom-blog')) %}
+    {% for feed in atom_feeds(for_page=site.get('/custom-blog'), alt='_primary') %}
         {{ feed.feed_id }}
     {% endfor %}
     '''
@@ -146,9 +186,18 @@ def test_discover_local(pad):
     assert feeds_blog == set(['feed-three', 'feed-four'])
 
     template_noblog = r'''
-    {% for feed in atom_feeds(for_page=site.get('/no-feed-content')) %}
+    {% for feed in atom_feeds(for_page=site.get('/no-feed-content'), alt='_primary') %}
         {{ feed.feed_id }}
     {% endfor %}
     '''
     feeds_noblog = feeds_from_template(pad, template_noblog)
     assert len(feeds_noblog) == 0
+
+
+def test_localized_config(pad):
+    plugin = pad.env.plugins['atom']
+    assert plugin.get_atom_config('feed-five', 'name') \
+        == 'Feed Five'
+    assert plugin.get_atom_config('feed-five', 'name', alt='de') \
+        == u'Feed Fünf'
+
